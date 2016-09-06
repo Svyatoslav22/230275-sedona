51cf375170b9fafdb0d0a37779d55a2718b63355
Svyatoslav22 <svt93@bk.ru>
2016-09-06 19:35:59 +0500

    я почти все

--- js/main.js
+++ js/main.js
@@ -2,4 +2,5 @@ var link = document.querySelector(".main-ul button");
 var popup = document.querySelector(".search-ul");
 link.addEventListener("click", function (event) {
     popup.classList.toggle("hidden-block");
+    popup.classList.toggle("search-ul");
 });
